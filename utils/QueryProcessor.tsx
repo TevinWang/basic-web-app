4041a2c900ef5cf4d5051792f2d523e84ffd724e
Tevin Wang <tevin@cmu.edu>
2024-03-14 16:33:38 -0400

    Update QueryProcessor.tsx

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -69,7 +69,7 @@ export default function QueryProcessor(query: string): string {
 
    if (query.toLowerCase().includes("primes")) {
     const values = query.match(/\d+/g);
-    var sum = [];
+    var primes = [];
     if (values != null) {
       const numValues = [...values.map(Number)]
       numValues.forEach((value) => {
@@ -85,10 +85,10 @@ export default function QueryProcessor(query: string): string {
         }
 
         if (isPrime) {
-          sum.append(value.toString());
+          primes.append(value.toString());
         }
       });
-      return sum.join(',');
+      return primes.join(',');
     } else {
       return "0";
     }
